54a326635ed3fb7927d82e41e17575ebe2cce838
aevilesaguiar <aeviles@hotmail.com>
2022-05-28 13:54:26 -0300

    Initial commit

---
+++ src/main/java/com/aeviles/deliveryapi/DeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.aeviles.deliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeviles/deliveryapi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeviles.deliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
